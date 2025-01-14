d4dbe7ead4c9e28af52d8f57920c0b05c86c71a8
Kenny Tang <kenny.t4ng@gmail.com>
2025-01-14 22:11:40 +0700

    Add Button

--- src/simulation.java
+++ src/simulation.java
@@ -3,6 +3,7 @@ import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
@@ -27,63 +28,12 @@ public class simulation extends Application {
     public static void main(String[] args){
         launch(args);
     }
-
+    
     @Override
     public void start(Stage stage) throws Exception{
         Group root = new Group();
         Scene scene = new Scene(root,1280,720, Color.LAVENDER);
-
-        Text text = new Text();
-        text.setText("Turing Machine Simulation");
-        text.setX(340);
-        text.setY(65);
-        text.setFont(Font.font("Poppins",FontWeight.BOLD, 50));
-        text.setFill(Color.GRAY);
-
-        Line line = new Line();
-        line.setStartX(100);
-        line.setStartY(100);
-        line.setEndX(1180);
-        line.setEndY(100);
-        line.setStrokeWidth(2);
-        line.setStroke(Color.GREY);
-
-        Polygon triangle = new Polygon();
-        triangle.getPoints().setAll(
-            25.0,150.0,
-            55.0,150.0,
-            40.0,175.0);
-        triangle.setFill(Color.GREY);
-        triangle.setStroke(Color.BLACK);
-
-        for (int i = 0; i < cell_number; i++) {
-            Rectangle rect = new Rectangle(cell_width * i, 200, cell_width, cell_height);
-            rect.setFill(Color.WHITE);
-            rect.setStroke(Color.BLACK);
-            root.getChildren().add(rect);
-        }
-
-        for (int i = 0; i < inputList.size(); i++) {
-            String symbol = inputList.get(i);
-            Text input_tape = new Text(cell_width * i + 30, 250, symbol);
-            input_tape.setFont(Font.font("Arial",35));
-            root.getChildren().add(input_tape);
-        }
-
-        Image icon = new Image("TM_icon.png");
-
-        stage.getIcons().add(icon);
-        stage.setTitle("Turing Machine Simulation");
-        stage.setWidth(1280);
-        stage.setHeight(720);
-        stage.setResizable(false);
-
-        root.getChildren().add(text);
-        root.getChildren().add(line);
-        root.getChildren().add(triangle);
-        stage.setScene(scene);
-        stage.show();
-
+        List<Text> inputTapeNodes = new ArrayList<>();
         Map<String, Map<String,List<String>>> transition = new HashMap<>();
         
         Map<String,List<String>> q0 = new HashMap<>();
@@ -111,46 +61,144 @@ public class simulation extends Application {
         transition.put("q2", q2);
         transition.put("q3", q3);
 
-        try{
-            while (true){
+        Text text = new Text();
+        text.setText("Turing Machine Simulation");
+        text.setX(340);
+        text.setY(65);
+        text.setFont(Font.font("Poppins",FontWeight.BOLD, 50));
+        text.setFill(Color.GRAY);
+
+        Line line = new Line();
+        line.setStartX(100);
+        line.setStartY(100);
+        line.setEndX(1180);
+        line.setEndY(100);
+        line.setStrokeWidth(2);
+        line.setStroke(Color.GREY);
+
+        Polygon triangle = new Polygon();
+        triangle.getPoints().setAll(
+            25.0,150.0,
+            55.0,150.0,
+            40.0,175.0);
+        triangle.setFill(Color.GREY);
+        triangle.setStroke(Color.BLACK);
+
+        Rectangle button = new Rectangle();
+        button.setX(550);
+        button.setY(550);
+        button.setHeight(75);
+        button.setWidth(175);
+        button.setStroke(Color.BLACK);
+        button.setStrokeWidth(3);
+        button.setFill(Color.LAVENDERBLUSH);
+        button.setOpacity(0.5);
+
+        Text begin = new Text();
+        begin.setText("START");
+        begin.setX(575);
+        begin.setY(600);
+        begin.setFont(Font.font("Poppins",FontWeight.BOLD, 40));
+        begin.setFill(Color.GRAY);
+
+        button.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                System.out.println("Left mouse button clicked on the rectangle!");
+                button.setFill(Color.LIGHTGREEN);
+                runTuringMachine(transition, root, inputTapeNodes);
+            } else if (event.getButton() == MouseButton.SECONDARY) {
+                System.out.println("Right mouse button clicked on the rectangle!");
+                button.setFill(Color.RED); // Change color on right-click
+            }
+        });
+        
+        begin.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                System.out.println("Left mouse button clicked on the text!");
+                button.setFill(Color.LIGHTGREEN);
+                runTuringMachine(transition, root, inputTapeNodes);
+            } else if (event.getButton() == MouseButton.SECONDARY) {
+                System.out.println("Right mouse button clicked on the text!");
+                button.setFill(Color.RED); 
+            }
+        });
+
+        for (int i = 0; i < cell_number; i++) {
+            Rectangle rect = new Rectangle(cell_width * i, 200, cell_width, cell_height);
+            rect.setFill(Color.WHITE);
+            rect.setStroke(Color.BLACK);
+            root.getChildren().add(rect);
+        }
+
+        for (int i = 0; i < inputList.size(); i++) {
+            String symbol = inputList.get(i);
+            Text input_tape = new Text(cell_width * i + 30, 250, symbol);
+            input_tape.setFont(Font.font("Arial",35));
+            root.getChildren().add(input_tape);
+            inputTapeNodes.add(input_tape);
+        }
+
+        Image icon = new Image("TM_icon.png");
+
+        stage.getIcons().add(icon);
+        stage.setTitle("Turing Machine Simulation");
+        stage.setWidth(1280);
+        stage.setHeight(720);
+        stage.setResizable(false);
+
+        root.getChildren().add(text);
+        root.getChildren().add(line);
+        root.getChildren().add(triangle);
+        root.getChildren().add(button);
+        root.getChildren().add(begin);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void runTuringMachine(Map<String, Map<String, List<String>>> transition, Group root, List<Text> inputTapeNodes){
+
+        for (Text node : inputTapeNodes) {
+            root.getChildren().remove(node);
+        }
+
+        try {
+            while (true) {
                 String symbol = inputList.get(head_position);
-
-                // new state as key with transition function as value 
-                Map<String,List<String>> new_state = transition.get(current_state);
+    
+                // New state as key with transition function as value
+                Map<String, List<String>> new_state = transition.get(current_state);
                 List<String> transitionList = new_state.get(symbol);
-
+    
                 current_state = transitionList.get(0);
                 inputList.set(head_position, transitionList.get(1));
-                if (transitionList.get(2).toUpperCase() == "R"){
+                if (transitionList.get(2).equalsIgnoreCase("R")) {
                     head_position += 1;
-                } else if (transitionList.get(2).toUpperCase() == "L"){
+                } else if (transitionList.get(2).equalsIgnoreCase("L")) {
                     head_position -= 1;
-                } else{
+                } else {
                     System.out.println("Please input either R for right or L for Left ");
-                };
-                
-                if (head_position >= inputList.size()){
+                }
+    
+                if (head_position >= inputList.size()) {
                     inputList.add(blank_symbol);
                 }
-
+    
                 System.out.println(inputList);
                 System.out.println("Head Position : " + head_position);
                 System.out.println("Current State : " + current_state);
-                if (current_state == accept_state){
+                if (current_state.equals(accept_state)) {
                     System.out.println("Input Accepted\n");
                     break;
-                }else if (current_state == reject_state){
+                } else if (current_state.equals(reject_state)) {
                     System.out.println("Input Rejected\n");
                     break;
-                }
-                else{
+                } else {
                     System.out.println("Running...\n");
-                    if (head_position == inputList.size() + 1){
+                    if (head_position == inputList.size() + 1) {
                         System.out.println("Your Algorithm cannot Halt...\n");
                         break;
                     }
                 }
-                // Thread.sleep(300); 
             }
         } catch (NullPointerException e) {
             System.out.println("Input Rejected\n");
